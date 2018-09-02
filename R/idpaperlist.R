1eae265be3b0f2d424b826ffbaf2fbb176f08805
Aboozar Hadavand <hadavand.a@gmail.com>
2018-09-01 20:34:56 -0400

    fixed the paperlist function for some of the ids with only 1 paper

--- R/idpaperlist.R
+++ R/idpaperlist.R
@@ -5,15 +5,16 @@ idpaperlist <- function(x){
     lmin <- min(which(a %in% c("paper", "article", "chapter"))) - 2
     lmax <- max(which(a %in% c("paper", "article", "chapter"))) + 1
     amod <- a[lmin:lmax]
-    tmp <- data.frame(
-        X=amod,
-        ind=rep(1:4, length(amod)/4)
-    )
-    at <- unstack(tmp, X~ind)
+    # tmp <- data.frame(
+    #     X=amod,
+    #     ind=rep(1:4, length(amod)/4)
+    # )
+    # at <- unstack(tmp, X~ind)
+    at <- as.data.frame(matrix(amod,ncol=4, byrow=TRUE))
     colnames(at) <- c("publicationyear", "publicationtitle", "publicationtype", "publicationcitations")
     repind <- grep("This paper has another version", at$publicationtitle)
-    at <- at[-repind, ]
-    at$publicationyear <- as.numeric(at$publicationyear)
+    if (length(repind)!=0) at <- at[-repind, ]
+    # at$publicationyear <- as.numeric(at$publicationyear)
     at$publicationcitations <- as.numeric(at$publicationcitations)
     return(at)
 }
